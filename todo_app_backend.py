20499d01cad44d244ed0e29d582d02ee578a7eac
Likhitha R <89694994+likhitha0918@users.noreply.github.com>
2023-12-31 09:42:57 +0530

    Update todo_app_backend.py

--- todo_app_backend.py
+++ todo_app_backend.py
@@ -1,23 +1,22 @@
-# todo_app_backend.py
 import random
+from flask import Flask, jsonify  # Import Flask and jsonify here
 
 class ToDoBackend:
     def __init__(self):
         self.tasks = []
 
     # ... (backend logic)
-    # todo_app_backend.py
-    from flask import Flask, jsonify
 
-    app = Flask(__name__)
-    backend = ToDoBackend()
+# Create Flask app instance
+app = Flask(__name__)
+backend = ToDoBackend()  # Instantiate ToDoBackend outside of __main__
 
 # API endpoint to get tasks
-    @app.route("/tasks", methods=["GET"])
-    def get_tasks():
-        return jsonify({"tasks": backend.tasks})
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify({"tasks": backend.tasks})
 
 # Other API endpoints for adding, deleting, etc.
 
-    if __name__ == "__main__":
-        app.run()
+if __name__ == "__main__":
+    app.run()
